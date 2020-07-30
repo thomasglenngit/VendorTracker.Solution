9d1fdf5c1f9e64c90e3d59f93744681e88252a40
Thomas Glenn <thomasgrantglenn@mac.com>
2020-07-29 23:10:22 -0600

    add failed test for Vendor GetAll VendorList method.

--- VendorTracker.Tests/ModelTests/VendorTests.cs
+++ VendorTracker.Tests/ModelTests/VendorTests.cs
@@ -51,22 +51,23 @@ namespace VendorTracker.Tests
       Assert.AreEqual(1, result);
     }
 
-    // [TestMethod]
-    // public void GetAll_ReturnsAllVendorObjects_VendorList()
-    // {
-    //   //Arrange
-    //   string name01 = "Work";
-    //   string name02 = "School";
-    //   Vendor newVendor1 = new Vendor(name01);
-    //   Vendor newVendor2 = new Vendor(name02);
-    //   List<Vendor> newList = new List<Vendor> { newVendor1, newVendor2 };
+    [TestMethod]
+    public void GetAll_ReturnsAllVendorObjects_VendorList()
+    {
+      //Arrange
+      string name01 = "Work";
+      string name02 = "School";
+      string description = "Fun stuff";
+      Vendor newVendor1 = new Vendor(name01, );
+      Vendor newVendor2 = new Vendor(name02);
+      List<Vendor> newList = new List<Vendor> { newVendor1, newVendor2 };
 
-    //   //Act
-    //   List<Vendor> result = Vendor.GetAll();
+      //Act
+      List<Vendor> result = Vendor.GetAll();
 
-    //   //Assert
-    //   CollectionAssert.AreEqual(newList, result);
-    // }
+      //Assert
+      CollectionAssert.AreEqual(newList, result);
+    }
 
     // [TestMethod]
     // public void Find_ReturnsCorrectVendor_Vendor()
